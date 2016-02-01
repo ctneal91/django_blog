a4bc265c7c9235005e4fa4de642597847b0c4669
Christian Neal <c.neal91@gmail.com>
2016-02-01 12:45:06 -0600

    Create templates for index anddetail pages

--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    context = {'latest_post_list': latest_post_list}
+    return render(request, 'myblog/index.html', context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'myblog/detail.html', {'post': post})
